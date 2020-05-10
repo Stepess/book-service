ead24324211b3384aa650aefe7457cf36098603a
Stepess <stepanersh@gmail.com>
2020-05-10 14:57:25 +0300

    initial commit

---
+++ src/test/java/ua/micro/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.micro.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
